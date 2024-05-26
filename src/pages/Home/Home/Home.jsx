184dca0718228d3da0106eeb3d9ce0d1106157be
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2024-05-27 02:38:49 +0600

    feat: add custom hook for data fetching and integrate it into menu page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Office Lunch | Home</title>
+            </Helmet>
             <h2>This is home.</h2>
         </div>
     );
