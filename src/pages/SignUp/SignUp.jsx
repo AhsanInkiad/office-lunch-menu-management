de9566c4ebb4f1ff79d47624764a4d8ca864abd6
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2024-05-28 22:46:02 +0600

    fix: reset sign-up page and navigate to home after successful sign-up

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -3,18 +3,36 @@ import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../../providers/AuthProvider";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const {createUser} = useContext(AuthContext);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const {createUser, updateUserProfile} = useContext(AuthContext);
+   
+    const navigate = useNavigate();
+   
     const onSubmit = data => {
         console.log(data);
         createUser(data.email, data.password)
         .then(result => {
             const loggedUser = result.user;
             console.log(loggedUser);
+            updateUserProfile(data.name)
+            .then(()=>{
+                console.log('user profile info updated.')
+                reset()
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "User created successfully!",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+                  navigate('/');
+            })
+            .catch(error => console.log(error))
         })
 
     };
